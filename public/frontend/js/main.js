4b12e672a74e0c6705fece7ddfb7835bb59725f4
minhthuan1110 <thuanxieuxao2k@gmail.com>
2021-08-18 17:06:26 +0700

    fix bug dang nhap

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -265,5 +265,19 @@ $('.review__slide').slick({
 //         panel.classList.add('active')
 //     }
 // });
+var onChange = document.querySelectorAll('.form-header__action');
+  var onChange1 = document.querySelectorAll('.form-body');
+  for(var i=0; i<onChange.length;i++){
+    const addactive = onChange1[i];
+    onChange[i].onclick = function(){
+      var onChange1 = document.querySelector('.form-body.active');
+      for(var i=0; i<onChange.length;i++){
+        onChange[i].classList.remove('active');
+        onChange1.classList.remove('active');
+      }
+      this.classList.add('active');
+      addactive.classList.add('active');
+    }
+  }
 
 // // ====================== tab login================
